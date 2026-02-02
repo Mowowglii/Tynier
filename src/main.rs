cf3415f56e53ff4db95de0c90a5e575f58893b61
Mowowglii <erwann.razatovo@gmail.com>
2026-02-02 17:23:10 +0100

    Main could now run either compression or decompression on a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
+use std::path::Path;
+use anyhow::Result;
+use std::fs;
+
 mod compress;
 mod decompress;
 mod fhandler;
 mod cli;
 
-fn main() {
-    println!("Hello, world!");
+use cli::*;
+use crate::{decompress::{decomp}, fhandler::{generate_output, get_file_data}, compress::SlidingWindow};
+
+fn main() -> Result<()>{
+    let cli = cli::cli_parse();
+    match &cli.subcom {
+        SubComs::Compress{path, large, medium, small} => {
+            let p = Path::new(path);
+            let capacity = if *large {
+                32000usize
+            } else if *medium {
+                16000usize
+            } else if *small {
+                8000usize
+            } else {
+                4000usize
+            };
+            let mut buff : Vec<u8> = Vec::new();
+            get_file_data(p, &mut buff)?;
+            let mut sw = SlidingWindow::new(capacity, buff);
+            let (f, e) = generate_output(p, false, None)?.unwrap();
+            sw.compress(&f, e)?;
+            Ok(())
+        },
+        SubComs::Decompress{path} => {
+            let p = Path::new(path);
+            decomp(p)?;
+            Ok(())
+        }
+    }
 }
