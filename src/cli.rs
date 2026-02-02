b7c2316a11f328f81bb549485eec43b300cd4331
Mowowglii <erwann.razatovo@gmail.com>
2026-02-02 17:19:21 +0100

    Modification on the compress flags (Permit selection of the Sliding Window Size)

--- src/cli.rs
+++ src/cli.rs
@@ -5,16 +5,28 @@ use clap::{Parser, Subcommand};
 pub struct Cli{
     // Compress or Decompress subcommand
     #[command(subcommand)]
-    subcom : SubComs
+    pub subcom : SubComs
 }
 
 #[derive(Subcommand, Debug)]
-enum SubComs {
-    // Compress Subcommand
+pub enum SubComs {
+    /// Compress the file given in argument
     Compress {
-        path : String
+        path : String,
+        
+        /// large Window size for compression
+        #[arg(short, long)]
+        large : bool,
+
+        /// Medium Window size for compression
+        #[arg(short, long)]
+        medium : bool,
+
+        /// Small Window size for compression
+        #[arg(short, long)]
+        small : bool
     },
-    // Decompress Subcommand
+    /// Decompress the file given in argument
     Decompress {
         path : String
     }
