aa266c3b570ed5eb8e2e74c90f9b9f5e22f54e72
Mowowglii <erwann.razatovo@gmail.com>
2026-02-02 17:24:33 +0100

    cargo fmt command passed on all files

--- src/cli.rs
+++ src/cli.rs
@@ -2,34 +2,32 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, propagate_version=true)]
-pub struct Cli{
+pub struct Cli {
     // Compress or Decompress subcommand
     #[command(subcommand)]
-    pub subcom : SubComs
+    pub subcom: SubComs,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum SubComs {
     /// Compress the file given in argument
     Compress {
-        path : String,
-        
+        path: String,
+
         /// large Window size for compression
         #[arg(short, long)]
-        large : bool,
+        large: bool,
 
         /// Medium Window size for compression
         #[arg(short, long)]
-        medium : bool,
+        medium: bool,
 
         /// Small Window size for compression
         #[arg(short, long)]
-        small : bool
+        small: bool,
     },
     /// Decompress the file given in argument
-    Decompress {
-        path : String
-    }
+    Decompress { path: String },
 }
 
 pub fn cli_parse() -> Cli {
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use std::path::Path;
 use anyhow::Result;
 use std::fs;
+use std::path::Path;
 
+mod cli;
 mod compress;
 mod decompress;
 mod fhandler;
-mod cli;
 
+use crate::{
+    compress::SlidingWindow,
+    decompress::decomp,
+    fhandler::{generate_output, get_file_data},
+};
 use cli::*;
-use crate::{decompress::{decomp}, fhandler::{generate_output, get_file_data}, compress::SlidingWindow};
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let cli = cli::cli_parse();
     match &cli.subcom {
-        SubComs::Compress{path, large, medium, small} => {
+        SubComs::Compress {
+            path,
+            large,
+            medium,
+            small,
+        } => {
             let p = Path::new(path);
             let capacity = if *large {
                 32000usize
@@ -24,14 +33,14 @@ fn main() -> Result<()>{
             } else {
                 4000usize
             };
-            let mut buff : Vec<u8> = Vec::new();
+            let mut buff: Vec<u8> = Vec::new();
             get_file_data(p, &mut buff)?;
             let mut sw = SlidingWindow::new(capacity, buff);
             let (f, e) = generate_output(p, false, None)?.unwrap();
             sw.compress(&f, e)?;
             Ok(())
-        },
-        SubComs::Decompress{path} => {
+        }
+        SubComs::Decompress { path } => {
             let p = Path::new(path);
             decomp(p)?;
             Ok(())
